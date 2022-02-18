fa734bb990ed65934cff5cf2fa17d91678abd96b
HarunEnsar <99835656+HarunEnsar@users.noreply.github.com>
2022-02-18 11:34:40 +0300

    Add files via upload
    daire alan eklendi

--- Ucgen.java
+++ Ucgen.java
@@ -12,5 +12,19 @@ public class Ucgen {
         double u = (a + b + c) / 2;
         double alan=Math.sqrt(u * (u - a) * (u - b)*(u - c));
         System.out.println("Alan:"+alan);
+        System.out.println("\n\n\nDAİRENİN ALANI");
+        int ycap,aci;
+        System.out.print("Dairenin yarıçapı:");
+        ycap= girdi.nextInt();
+        System.out.print("Merkez açsının ölçüsü:");
+        aci= girdi.nextInt();
+        double pi=3.14;
+        double dAlan=(pi*ycap*ycap*aci)/360;
+        System.out.println("Dairenin alanı:"+dAlan);
+
+
+
+
+
     }
 }
